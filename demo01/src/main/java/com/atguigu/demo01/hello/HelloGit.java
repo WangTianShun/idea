c55e3468a62170965281b1d970ab095e193fb1cc
WangTianShun <563540326@qq.com>
2020-08-04 21:05:10 +0800

    创建分支

--- demo01/src/main/java/com/atguigu/demo01/hello/HelloGit.java
+++ demo01/src/main/java/com/atguigu/demo01/hello/HelloGit.java
@@ -3,5 +3,6 @@ package com.atguigu.demo01.hello;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("创建分支");
     }
 }
